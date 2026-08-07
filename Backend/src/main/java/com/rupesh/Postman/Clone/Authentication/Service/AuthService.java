ed4109250c015c74a4b73d41f770f20bfbb039b9
Rupesh377 <kr.rupesh1604@gmail.com>
2026-08-07 21:21:04 +0530

    minor fixes

--- Backend/src/main/java/com/rupesh/Postman/Clone/Authentication/Service/AuthService.java
+++ Backend/src/main/java/com/rupesh/Postman/Clone/Authentication/Service/AuthService.java
@@ -14,7 +14,6 @@ import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
