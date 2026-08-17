64e0bed2be7a78de38c0916438bb33a509e11aea
Rupesh377 <kr.rupesh1604@gmail.com>
2026-08-17 23:01:56 +0530

    Testing of workspace done

--- Backend/src/main/java/com/rupesh/Postman/Clone/Workspace/WorkspaceRepository.java
+++ Backend/src/main/java/com/rupesh/Postman/Clone/Workspace/WorkspaceRepository.java
@@ -1,13 +1,18 @@
 package com.rupesh.Postman.Clone.Workspace;
 
 import com.rupesh.Postman.Clone.Authentication.Entity.User;
+import org.hibernate.jdbc.Work;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.swing.text.html.Option;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface WorkspaceRepository extends JpaRepository<Workspace, Long> {
 
     List<Workspace> findByOwner(User owner);
+
+    Boolean existsByName(String name);
 }
